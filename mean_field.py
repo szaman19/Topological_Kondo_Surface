21a376ae0a52807f53ea937fdcc822fc6283fbb5
Shehtab Zaman <szaman5@binghamton.edu>
2019-07-29 00:10:25 -0400

    debug

--- mean_field.py
+++ mean_field.py
@@ -219,7 +219,7 @@ def main():
 	params['mu_f_delta'] = 1
 	params['mu_c'] = .2
 	params['Xi_guess'] = 1
-	params['cutoff'] = 50
-	params['cutoff_norm'] = 10
+	params['cutoff'] = 100
+	params['cutoff_norm'] = 20
 	self_consistent(params)
 main()
