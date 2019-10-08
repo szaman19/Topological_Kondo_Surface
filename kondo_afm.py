d4f2f2771c9ef027c06c014da8f15a0cfb3b52e7
Shehtab Zaman <shehtabzaman@gmail.com>
2019-10-07 21:19:22 -0400

    I think I fixed it

--- kondo_afm.py
+++ kondo_afm.py
@@ -204,7 +204,7 @@ def calibrate_mu(op, params):
 		is_equal_NC = util_equal(conduction_number,1)
 		is_equal_NF = util_equal(moment_number,1)
 		loop_condition = not (is_equal_NF and is_equal_NC)
-		if(counter % 10 == 0):
+		if(counter % 100 == 0):
 			print("N_c: {:.9f}, N_f: {:.9f}".format( conduction_number, moment_number))
 		counter +=1
 	# print(util_equal(conduction_number,1))
