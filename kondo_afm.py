2229af39de89c6ea95a480e52157eb6046ec6b7d
Shehtab Zaman <shehtabzaman@gmail.com>
2019-10-15 09:37:39 -0400

    run with reversed signs

--- kondo_afm.py
+++ kondo_afm.py
@@ -669,8 +669,8 @@ def hamiltonian_order_params(hamiltonian, order_params):
 	#H2 Block Begin ############################################################################## 
 	hamiltonian [0][4] = order_params['M2_f']
 	hamiltonian [1][5] = - order_params['M2_f']
-	hamiltonian [2][6] =  order_params['M2_c']
-	hamiltonian [3][7] = - order_params['M2_c']
+	hamiltonian [2][6] = - order_params['M2_c']
+	hamiltonian [3][7] =  order_params['M2_c']
 
 
 	hamiltonian [0][6] = - order_params['xi2_up']
@@ -681,8 +681,8 @@ def hamiltonian_order_params(hamiltonian, order_params):
 	#H3 Block Begin ############################################################################## 
 	hamiltonian [4][0] = order_params['M2_f']
 	hamiltonian [5][1] = - order_params['M2_f']
-	hamiltonian [6][2] = order_params['M2_c']
-	hamiltonian [7][3] = -order_params['M2_c']
+	hamiltonian [6][2] = -order_params['M2_c']
+	hamiltonian [7][3] = order_params['M2_c']
 
 
 	hamiltonian [4][2] = - order_params['xi2_up']
@@ -694,7 +694,7 @@ def hamiltonian_order_params(hamiltonian, order_params):
 
 def main():
 	K_POINTS = gen_brillouin_zone(20)
-	points = gen_brillouin_zone()
+	# points = gen_brillouin_zone()
 
 	NUM_PROCESS = 8
 
