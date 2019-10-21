35afcd628c366faef9a3f8c39c812da6c1944968
Shehtab Zaman <shehtabzaman@gmail.com>
2019-10-20 21:48:09 -0400

    Change to the mu_f updater

--- kondo_afm.py
+++ kondo_afm.py
@@ -651,8 +651,8 @@ def hamiltonian_order_params(hamiltonian, order_params):
 	#H1 Block Begin ############################################################################## 
 	hamiltonian [0][0] = hamiltonian [0][0] + order_params['M1_f']
 	hamiltonian [1][1] = hamiltonian [1][1] - order_params['M1_f']
-	hamiltonian [2][2] = hamiltonian [2][2] + order_params['M1_c']
-	hamiltonian [3][3] = hamiltonian [3][3] -  order_params['M1_c']
+	hamiltonian [2][2] = hamiltonian [2][2] - order_params['M1_c']
+	hamiltonian [3][3] = hamiltonian [3][3] +  order_params['M1_c']
 
 
 	hamiltonian [0][2] = - order_params['xi1_up']
@@ -663,8 +663,8 @@ def hamiltonian_order_params(hamiltonian, order_params):
 	#H4 Block Begin ##############################################################################
 	hamiltonian [4][4] = hamiltonian [4][4] + order_params['M1_f']
 	hamiltonian [5][5] = hamiltonian [5][5] - order_params['M1_f']
-	hamiltonian [6][6] = hamiltonian [6][6] + order_params['M1_c']
-	hamiltonian [7][7] = hamiltonian [7][7] - order_params['M1_c']
+	hamiltonian [6][6] = hamiltonian [6][6] - order_params['M1_c']
+	hamiltonian [7][7] = hamiltonian [7][7] + order_params['M1_c']
 
 
 	hamiltonian [4][6] = - order_params['xi1_up']
@@ -675,8 +675,8 @@ def hamiltonian_order_params(hamiltonian, order_params):
 	#H2 Block Begin ############################################################################## 
 	hamiltonian [0][4] = order_params['M2_f']
 	hamiltonian [1][5] = - order_params['M2_f']
-	hamiltonian [2][6] =  order_params['M2_c']
-	hamiltonian [3][7] = - order_params['M2_c']
+	hamiltonian [2][6] =  -order_params['M2_c']
+	hamiltonian [3][7] =  order_params['M2_c']
 
 
 	hamiltonian [0][6] = - order_params['xi2_up']
@@ -687,8 +687,8 @@ def hamiltonian_order_params(hamiltonian, order_params):
 	#H3 Block Begin ############################################################################## 
 	hamiltonian [4][0] = order_params['M2_f']
 	hamiltonian [5][1] = - order_params['M2_f']
-	hamiltonian [6][2] = order_params['M2_c']
-	hamiltonian [7][3] = - order_params['M2_c']
+	hamiltonian [6][2] = -order_params['M2_c']
+	hamiltonian [7][3] =  order_params['M2_c']
 
 
 	hamiltonian [4][2] = - order_params['xi2_up']
